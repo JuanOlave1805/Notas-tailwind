b27f73fb997df0c723038dc122d0ac8d3bfbf150
Juan Olave <143722476+JuanOlave1805@users.noreply.github.com>
2024-05-06 18:03:47 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+       'Color_prueba': '#70C97B',
+      },
+      spacing: {  //Propiedad de uso para espacios, no importa el espacio que sea
+        'medida' : '120px'
+      },
+      width: {  //Propiedad de uso exclusivo para la propiedad width
+        'medida' : '120px'
+      }
+    }
+  }
+}
